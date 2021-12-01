9198f0a727a57ba7ccee7a30dc88df483f10f839
Denys Anosov <anosovdenys@gmail.com>
2021-12-01 02:05:18 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from "rxjs/operators";
+import './App.css';
 
-function App() {
+
+const App: React.FC = () => {
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const [status, setStatus] = useState('not-ticking');
+
+  useEffect(() => {
+    const unsub = new Subject();
+
+    interval(1000)
+      .pipe(takeUntil(unsub))
+      .subscribe(() => {
+        if (timerOn) {
+          setTime(prevTime => prevTime + 1000)
+        }
+      });
+
+    return () => {
+      unsub.next(0);
+      unsub.complete();
+    };
+  }, [timerOn]);
+
+  const handleStart = () => {
+    setTimerOn(prevTimerState => !prevTimerState);
+    setStatus('ticking');
+  }
+
+  const handleStop = () => {
+    setTime(0);
+    setTimerOn(false);
+    setStatus('not-ticking');
+  }
+
+  const handleReset = () => {
+    if (timerOn && status === 'ticking') {
+      handleStop();
+      handleStart();
+    }
+  }
+
+  const oneSec = time / 1000;
+
+  const formatTime = (durationInSeconds: number) => {
+    const hours = Math.floor(durationInSeconds / 3600);
+    const minutes = Math.floor((durationInSeconds % 3600) / 60);
+    const seconds = durationInSeconds - hours * 3600 - minutes * 60;
+
+    return `${hours}h ${minutes}m ${seconds}s`;
+  }
+
   return (
     <div className="App">
-      12345
+      <span
+        className="Time"
+      >
+        {formatTime(oneSec)}
+      </span>
+      <div>
+        {status === 'not-ticking' && (
+          <button
+            type="button"
+            onClick={handleStart}
+            className="Button"
+          >
+            Start
+          </button>
+        )}
+        {status === 'ticking' && (
+          <button
+            type="button"
+            onClick={handleStop}
+            className="Button"
+          >
+            Stop
+          </button>
+        )}
+        <button
+          type="button"
+          onClick={handleReset}
+          className="Button"
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
